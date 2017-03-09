a1485b08f9d3514c71dab333938ed3b58a5fd451
sevrukandrey <Dubinni89>
2017-03-09 20:41:03 +0200

    1234

---
+++ src/test/java/Test1.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+        import org.junit.Test;
+        import sun.net.www.http.HttpClient;
+
+        import java.net.HttpURLConnection;
+
+/**
+ * Created by User on 09.03.2017.
+ */
+public class Test1 {
+
+    @Test
+    public void firstTest() {
+        System.out.println("first test");
+        Assert.assertFalse(false);
+    }
+}
